cf124f253a400e20fabadffc840a6f6967890b50
Wellington Santos <well.santos1334@gmail.com>
2024-05-01 12:11:05 -0300

    feat: date format file

---
+++ src/shared/constants/DATES.ts
@@ -0,0 +1,11 @@
+export const DATES = {
+  DATE: 'yyyy-MM-dd',
+  HOUR: 'HH:mm',
+  DATE_TIME: 'yyyy-MM-dd HH:mm:ss',
+  DATE_TIME_BR: 'dd-MM-yyyy HH:mm:ss',
+  DATE_BR: 'dd-MM-yyyy',
+};
+
+export const TIMEZONES = {
+  AMERICA_SAO_PAULO: 'America/Sao_Paulo',
+};

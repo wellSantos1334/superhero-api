977d34d47bb2cacfa74c7de6e740f84f749c41ff
Wellington Santos <well.santos1334@gmail.com>
2024-05-04 13:05:22 -0300

    feat: create user seed

---
+++ src/shared/infra/typeorm/seeds/1714690630-CreateUser.seeder.ts
@@ -0,0 +1,20 @@
+import { type Seeder } from 'typeorm-extension';
+import { type DataSource } from 'typeorm';
+
+import { User } from '../../../../modules/users/infra/typeorm/entities/User';
+import { hashPassword } from '../../../util/Password';
+
+export default class UserSeeder implements Seeder {
+  public async run(dataSource: DataSource): Promise<void> {
+    const userRepository = dataSource.getRepository(User);
+
+    await userRepository.save({
+      id: 'a606d75b-2cc4-451a-85ed-9a31fe5b8bd4',
+      name: 'userName',
+      cpf: '12250060040',
+      email: 'email@teste.com',
+      biography: 'biography description',
+      password: hashPassword('123123@Ssr'),
+    });
+  }
+}
